290595b9c47416ad530f16acb879c072253f1c2f
Lindaagstna <lindaagstna11@gmail.com>
2025-05-08 15:37:09 +0700

    change login

--- dbservis/Layouts/navbar.php
+++ dbservis/Layouts/navbar.php
@@ -140,20 +140,17 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="dist/img/loginv2.jpg" alt="GoMontir" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">GoMontir</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="dist/img/marklee.png" class="img-circle elevation-2" alt="User Image">
-            </div>
             <div class="info">
-                <a href="#" class="d-block">Mark Lee</a>
+                <a href="#" class="d-block">Servis Kendaraan</a>
             </div>
         </div>
 
@@ -179,6 +176,14 @@
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="?url=#" class="nav-link <?php if($file == 'login') echo 'active' ?>">
+                        <i class="nav-icon fas fa-user"></i>
+                        <p>
+                           Login
                         </p>
                     </a>
                 </li>
