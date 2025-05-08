9ee70701424e757979d12242b2143ec02f2326a4
Lindaagstna <lindaagstna11@gmail.com>
2025-05-08 16:12:38 +0700

    change login2

--- dbservis/Layouts/navbar.php
+++ dbservis/Layouts/navbar.php
@@ -180,7 +180,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="?url=#" class="nav-link <?php if($file == 'login') echo 'active' ?>">
+                    <a href="?url=dbservis\templates\examples\login.html" class="nav-link <?php if($file == 'dbservis\templates\examples\login.html') echo 'active' ?>">
                         <i class="nav-icon fas fa-user"></i>
                         <p>
                            Login
